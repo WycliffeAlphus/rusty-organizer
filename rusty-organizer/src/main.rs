9e025f3c7461dc937ee2ace5ff4b88b15e2ec44e
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-05-30 23:34:01 +0300

    feat:add main

--- rusty-organizer/src/main.rs
+++ rusty-organizer/src/main.rs
@@ -21,3 +21,26 @@ enum OrganizeMode {
     about = "Rusty Organizer - A simple CLI tool to sort files by type/extension",
     long_about = None
 )]
+
+struct Args {
+
+    #[arg(short, long, default_value_t = OrganizeMode::Type)]
+
+    mode: OrganizeMode,
+
+    #[arg(short, long, default_value_t = false)]
+    dry_run: bool,
+
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
+}
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::pars();
+    let source_dir = Path::new(&args.source);
+
+    if !source_dir.exists() {
+        resturn Err(format!("Source directory '{}' doesn't exist", args.source).into())
+    }
+}
